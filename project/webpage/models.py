64f36c30c2fbcd6ca58f945afa6952672fad0d70
Anders Kvamme Felde <49548776+AndersFelde@users.noreply.github.com>
2021-04-23 10:34:49 +0200

    Initial commit

---
+++ project/webpage/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# from .submodels.model1 import Model1
+# from .submodels.model2 import Model2
+
+#  Husk å adde til admin.py
+
+#  python manage.py makemigrations webpage
+#  python mange.py migrate
